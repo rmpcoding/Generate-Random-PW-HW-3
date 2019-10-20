19139a3a6e2c4f3c68643a2a3cf3cd47b9832d43
Robert Parsons <rmpcoding@gmail.com>
2019-10-19 21:21:13 -0700

    password now appears in textContent area when button is clicked

--- script.js
+++ script.js
@@ -62,11 +62,9 @@ var upperCaseArr = [
 ];
 
 // create empty nested array
-// push only what user chooses into nested array
+// push only what user chooses into nested array. This will be for our result array inside a function later on.
 
 var nestedArr = [];
-
-// console.log(nestedArr);
 
 // ______________________________userPrompts below______________________________________________________ //
 // prompt user to choose between 8 and 128 for length of pw
@@ -83,42 +81,20 @@ function getLengthOfCharacters() {
   return prompt(askUserLengthOfCharacters);
 }
 
-// create variable to fetch returned value of user input. This is how I can get the variable outside of a function.
+// create variable to fetch returned value of user length input. This is how I can get the variable outside of a function.
 
 var userLength = getLengthOfCharacters();
 
-console.log(userLength);
+// create test function for user inputted characters.
 
-var userFailsCriteria =
-  "Sorry, you need to enter between 8 - 128 numeric characters. Please try again.";
-
-function testUserParameters() {
+function successfulPasswordLength() {
   if (parseInt(userLength) >= 8 && parseInt(userLength) <= 128) {
+    userPrompts(); //this is the main function that runs everything
+  } else {
     userPrompts();
-  } else if (parseInt(userLength) < 8 || parseInt(userLength) > 128) {
-    return prompt(userFailsCriteria);
-  } else if (typeof userLength === "NaN" || typeof userLength === "string") {
-    return prompt(userFailsCriteria);
   }
 }
-
-testUserParameters();
-
-// prompt(`You chose ${userLength} characters, but you need between 8 - 128 numeric characters. Please try again`);
-// prompt(`You chose ${userLength} characters, but you need between 8 - 128 numeric characters. Please try again`);
-
-// prompt("Please choose at least 8 characters, and no more than 128. Please try again.");
-// console.log(typeof userLengthNum);
-
-// else if (typeof userLength === "string") {
-//   prompt("I'm a string. Please choose at least 8 characters, and no more than 128. Please try again.");
-// }
-
-// var userLengthNum = parseInt(userLength);
-
-// console.log(typeof userLengthNum);
-
-// what if it's null (i.e. someone enters letters)
+successfulPasswordLength();
 
 // ______________________________userPrompts function below, along with conditionals for for loop_____________________________________________ //
 
@@ -190,74 +166,73 @@ function userPrompts() {
 
   for (var i = 0; i < userLength; i++) {
     // select random index of array
-    // select random index of inside array
+    // select random index of inner array
     // concat such character to userPassword
     userPassword = userPassword + nestedArr[randomNumber1][randomNumber2];
-
+    // if true
+    // iterate through loop
+    // if not true
+    // move on to next loop
     randomNumber1 = Math.floor(Math.random() * nestedArr.length);
     randomNumber2 = Math.floor(Math.random() * nestedArr[randomNumber1].length);
+
+    var displayPassWord = document
+      .getElementById("generate")
+      .addEventListener("click", function() {
+        document.getElementById("password").innerText = userPassword;
+      });
+
+    // select text box by id // document.getelementbyid
+    // update textContent with value of user password
+    // .innerText() --pass in userPassword
+
+    //create eventlistener to start rolling out the function when the user clicks on the generate password button.
+
+    // var generateClick = document.getElementById("generate").addEventListener("click", );
+
+    // var displayPassWord = document.getElementById("password").addEventListener("click")
+
+    // alert the user of the new password (result array)
   }
-
-  console.log(userPassword);
-
-  // select text box by id // document.getelementbyid
-  // update textContent with value of user password
-  // .innerText() --pass in userPassword
-
-  // if true
-  // iterate through loop
-  // if not true
-  // move on to next loop
-
-  // user inputs number (this is not housed within the userPrompt function)
-  // retrieve user-inputted number
-  // use that as length for randomized number generator
-  // iterate through nestedArr with both randomNumber1 and randomNumber2 as arguments
-
-  // __________acts as a function below becausae it's housed within the userPrompt function___________________________________
-
-  // if user confrims true to special characters, run loop to extract random special characters
-}
-
-// no need to call the function because it calls the function in the test parameters function.
-// userPrompts();
+  return console.log(userPassword);
+} //DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE DO NOT DELETE //
 
 // nested array is empty [] (descriptive name for array)
 // if user chooses true characters (any)
 // push array into empty nested array [].
 
-// Calling the function above.
+// user inputs number (this is not housed within the userPrompt function)
+// retrieve user-inputted number
+// use that as length for randomized number generator
+// iterate through nestedArr with both randomNumber1 and randomNumber2 as arguments
 
 // ___________________________________________________________________________________________________________________________________________________
 
-//create eventlistener to start rolling out the function when the user clicks on the generate password button.
+// Number(parseInt(userLength)) === NaN
+// else if (Number(userLength) === NaN) {
+//   return confirm(userFailsCriteria);
+// }
+// prompt(`You chose ${userLength} characters, but you need between 8 - 128 numeric characters. Please try again`);
+// prompt(`You chose ${userLength} characters, but you need between 8 - 128 numeric characters. Please try again`);
+// prompt("Please choose at least 8 characters, and no more than 128. Please try again.");
+// console.log(typeof userLengthNum);
+// else if (typeof userLength === "string") {
+//   prompt("I'm a string. Please choose at least 8 characters, and no more than 128. Please try again.");
+// }
+// what if it's null (i.e. someone enters letters)
 
-// var generateClick = document.getElementById("generate").addEventListener("click", randomLoop);
+// tried to create failsafe but it didn't work out.____________________________________________________________________________________________________________________________________________________
 
-// var displayPassWord = document.getElementById("password").addEventListener("click")
+// var userFailsCriteria =
+//   "Sorry, you need to enter between 8 - 128 numeric characters. Please try again.";
 
-// Assign numbers 0, 1, 2, 3 to arrays;
-
-// if (randomNumber === 0 && userSpecialCharacters === true) then push into array
-
-// if (randomNumber === 1 && userLowerCase === true) then push into array
-
-// if (randomNumber === 2 && userUpperCase === true) then push into array
-
-// remember to ensure the user selects at least one criteria
-
-// _________________create a function to assign numbers to arrays_______________________ //
-
-// alert the user of the new password (result array)
-
-// learn about scope / scoping
-
-// if all criteria are met.
-// if (userSpecialCharacters === true && userNumbers === true && userLowerCase === true && userUpperCase === true) {
-//   console.log("heyyyy, you can try the loop now");
+// function testUserParameters() {
+//   if (parseInt(userLength) < 8 || parseInt(userLength) > 128) {
+//     // alert(userFailsCriteria);
+//     getLengthOfCharacters();
+//   } else {
+//     successfulPasswordLength();
+//   }
 // }
 
-// specCharArr,
-// numbersArr,
-// lowerCaseArr,
-// upperCaseArr
+// learn about scope / scoping
