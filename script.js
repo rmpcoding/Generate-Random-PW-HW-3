f8a8fe26880f5e95f1dec24bf60b8458725dad61
Robert Parsons <rmpcoding@gmail.com>
2019-10-21 16:27:06 -0700

    Fixed the logic regarding userLength conditions

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
 // create arrays for characters (special characters, numbers, lowercase, uppercase)
 
-var specCharArr = [
-  "[", 
-  "]", 
-  "!", 
-  "/", 
-  "^", 
-  "$", 
-  "|", 
-  "?", 
-  "*", 
-  "+", 
-  "(", 
-  ")"
-  ];
-var numbersArr = [
-  "0", 
-  "1", 
-  "2", 
-  "3", 
-  "4", 
-  "5", 
-  "6", 
-  "7", 
-  "8", 
-  "9"
-  ];
+var specCharArr = ["[", "]", "!", "/", "^", "$", "|", "?", "*", "+", "(", ")"];
+var numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCaseArr = [
   "a",
   "b",
@@ -83,147 +59,132 @@ var upperCaseArr = [
   "Z"
 ];
 
-// create empty nested array
-// push only what user chooses into nested array. This will be for our result array inside a function later on.
+// DOM traversal variables below:
+var handleGeneratePassword = document.getElementById("generate");
+var textArea = document.getElementById("password");
+var copyPassword = document.getElementById("copy");
+
+// DOM addEventListeners below:
+
+handleGeneratePassword.addEventListener("click", generatePassword);
+
+// NEXT STEPS:
+// Once generate password button is clicked, I need to display the password in the text area content box. 
+
+copyPassword.onclick = function() {
+  textArea.select();
+  console.log("hello");
+  document.execCommand("copy");
+};
+
+// Create empty nested array, in which we will push characters from user-chosen criteria into such empty array.
+// This will be for our result array inside a function later on.
 
 var nestedArr = [];
 
 // prompt user to choose between 8 and 128 for length of pw
-// create function to return the value of user input
 
-var askUserLengthOfCharacters =
-"How many characters do you want? Choose between 8 - 128.";
+console.log("This is right above the generatePassword function");
 
-function getLengthOfCharacters() {
-  return prompt(askUserLengthOfCharacters);
-}
+function generatePassword() {
+  generatePassword;
+  var userLength = prompt(
+    "How many characters do you want? Choose between 8 - 128."
+  );
 
-// create variable to fetch returned value of user length input. This is how I can get the variable outside of a function.
+  userLength = parseInt(userLength);
+  var isValidLength = userLength < 8 || userLength > 128;
 
-var userLength = getLengthOfCharacters();
+  // create conditions for user inputted characters.
 
-// create test function for user inputted characters.
-
-function successfulPasswordLength() {
-  if (parseInt(userLength) >= 8 && parseInt(userLength) <= 128) {
+  if (isValidLength) {
+    generatePassword(); //starts the process over again
+  } else {
     userPrompts(); //this is the main function that runs everything
-  } else {
-    userPrompts(); //as you will see below, I couldn't get my test parameters working.
-  }
-}
-successfulPasswordLength();
-
-// tried to create failsafe but it didn't work out.____________________________________________________________________________________________________________________________________________________
-
-// prompt to try again if user doesn't meet criteria
-var userFailsCriteria =
-  "Sorry, you need to enter between 8 - 128 numeric characters. Please try again.";
-
-function testUserParameters() {
-  if (parseInt(userLength) < 8 || parseInt(userLength) > 128) {
-    // alert(userFailsCriteria);
-    getLengthOfCharacters();
-  } else {
-    successfulPasswordLength(); // I spent around two hours trying to figure this part out but couldn't :(
-  }
-}
-
-// ______________________________userPrompts main function below, along with conditionals for for loop_____________________________________________ //
-
-function userPrompts() {
-  // prompt user to decide yes/no for special characters
-  var userSpecialCharacters = confirm(
-    "Click Okay if you want to use special characters, or else click cancel"
-  );
-  console.log(userSpecialCharacters);
-
-  // defaults to checking whether the statement below is true
-  if (userSpecialCharacters) {
-    nestedArr.push(specCharArr);
   }
 
-  // prompt user to decide yes/no for numbers
-  var userNumbers = confirm(
-    "Click Okay if you want to use number characters, or else click cancel"
-  );
-  console.log(userNumbers);
+  // userPrompts main function below, along with conditionals for for loop_____________________________________________ //
 
-  // defaults to checking whether the statement below is true
-  if (userNumbers) {
-    nestedArr.push(numbersArr);
+  function userPrompts() {
+    // prompt user to decide yes/no for special characters
+    var userSpecialCharacters = confirm(
+      "Click Okay if you want to use special characters, or else click cancel"
+    );
+    console.log(userSpecialCharacters);
+
+    // defaults to checking whether the statement below is true
+    if (userSpecialCharacters) {
+      nestedArr.push(specCharArr);
+    }
+
+    // prompt user to decide yes/no for numbers
+    var userNumbers = confirm(
+      "Click Okay if you want to use number characters, or else click cancel"
+    );
+    console.log(userNumbers);
+
+    // defaults to checking whether the statement below is true
+    if (userNumbers) {
+      nestedArr.push(numbersArr);
+    }
+
+    // prompt user to decide yes/no for lower case letters
+    var userLowerCase = confirm(
+      "Click Okay if you want to use lowercase characters, or else click cancel"
+    );
+    console.log(userLowerCase);
+
+    // defaults to checking whether the statement below is true
+    if (userLowerCase) {
+      nestedArr.push(lowerCaseArr);
+    }
+
+    // prompt user to decide yes/no for upper case letters
+    var userUpperCase = confirm(
+      "Click Okay if you want to use uppercase characters, or else click cancel"
+    );
+    console.log(userUpperCase);
+
+    // defaults to checking whether the statement below is true
+    if (userUpperCase) {
+      nestedArr.push(upperCaseArr);
+    }
+
+    // if user chooses nothing, iterate over cycle to give user another chance to input correctly
+    if (
+      userSpecialCharacters === false &&
+      userNumbers === false &&
+      userLowerCase === false &&
+      userUpperCase === false
+    ) {
+      alert("Please okay a criterion least once");
+      userPrompts();
+    }
+
+    // if criterion meets true from user prompts (i.e., confirm if you want special characters)
+    // iterate through loop of such array
+    // if criterion not true
+    // move on to next
+
+    // generate a random number using math floor math random methods to iterate over four arrays
+    // select random index of array
+    // select random index of inner array
+    // concat (push) such character to userPassword (push characters into new string)
+
+    var randomNumber1 = Math.floor(Math.random() * nestedArr.length);
+    var randomNumber2 = Math.floor(
+      Math.random() * nestedArr[randomNumber1].length
+    );
+
+    var userPassword = "";
+
+    for (var i = 0; i < userLength; i++) {
+      userPassword = userPassword + nestedArr[randomNumber1][randomNumber2];
+      randomNumber1 = Math.floor(Math.random() * nestedArr.length);
+      randomNumber2 = Math.floor(
+        Math.random() * nestedArr[randomNumber1].length
+      );
+    }
+    return console.log(userPassword);
   }
-
-  // prompt user to decide yes/no for lower case letters
-  var userLowerCase = confirm(
-    "Click Okay if you want to use lowercase characters, or else click cancel"
-  );
-  console.log(userLowerCase);
-
-  // defaults to checking whether the statement below is true
-  if (userLowerCase) {
-    nestedArr.push(lowerCaseArr);
-  }
-
-  // prompt user to decide yes/no for upper case letters
-  var userUpperCase = confirm(
-    "Click Okay if you want to use uppercase characters, or else click cancel"
-  );
-  console.log(userUpperCase);
-
-  // defaults to checking whether the statement below is true
-  if (userUpperCase) {
-    nestedArr.push(upperCaseArr);
-  }
-
-  // if user chooses nothing, iterate over cycle to give user another chance to input correctly
-  if (
-    userSpecialCharacters === false &&
-    userNumbers === false &&
-    userLowerCase === false &&
-    userUpperCase === false
-  ) {
-    alert("Please okay a criterion least once");
-    userPrompts();
-  }
-
-  // if criterion meets true from user prompts (i.e., confirm if you want special characters)
-  // iterate through loop of such array
-  // if criterion not true
-  // move on to next
-
-  // generate a random number using math floor math random methods to iterate over four arrays
-  // select random index of array
-  // select random index of inner array
-  // concat (push) such character to userPassword (push characters into new string)
-
-  var randomNumber1 = Math.floor(Math.random() * nestedArr.length);
-  var randomNumber2 = Math.floor(
-    Math.random() * nestedArr[randomNumber1].length
-  );
-
-  var userPassword = "";
-
-  for (var i = 0; i < userLength; i++) {
-    userPassword = userPassword + nestedArr[randomNumber1][randomNumber2];
-    randomNumber1 = Math.floor(Math.random() * nestedArr.length);
-    randomNumber2 = Math.floor(Math.random() * nestedArr[randomNumber1].length);
-
-    // Click events and functions below:
-    // Create eventlistener to start rolling out the function when the user clicks on the generate password button.
-    var displayPassword = document
-      .getElementById("generate")
-      .addEventListener("click", function() {
-        document.getElementById("password").innerText = userPassword;
-      });
-
-    var textArea = document.getElementById("password");
-    var copyPassword = document.getElementById("copy");
-
-    copyPassword.onclick = function() {
-      textArea.select();
-      console.log("hello");
-      document.execCommand("copy");
-    };
-  }
-  return console.log(userPassword);
 }
