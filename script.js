1dd527a7f26466078712692289587f54102d17a0
Robert Parsons <rmpcoding@gmail.com>
2019-12-15 19:28:02 -0800

    cleaned up code completely. Turned one large function into multiple functions. Need to clean up generatePassword() function on line 115.

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ var copyPassword = document.getElementById("copy");
 handleGeneratePassword.addEventListener("click", generatePassword);
 
 // NEXT STEPS:
-// Once generate password button is clicked, I need to display the password in the text area content box. 
+// Once generate password button is clicked, display the password in the text area content box.
 
 copyPassword.onclick = function() {
   textArea.select();
@@ -80,111 +80,156 @@ copyPassword.onclick = function() {
 // Create empty nested array, in which we will push characters from user-chosen criteria into such empty array.
 // This will be for our result array inside a function later on.
 
+// VARIABLES MADE GLOBAL FOR FUNCTIONS BELOW
+// ===================================================================================================
 var nestedArr = [];
+var userSpecialCharacters;
+var userNumbers;
+var userLowerCase;
+var userUpperCase;
 
-// prompt user to choose between 8 and 128 for length of pw
 
 console.log("This is right above the generatePassword function");
 
-function generatePassword() {
-  generatePassword;
-  var userLength = prompt(
+
+// VALIDATE LENGTH FUNCTION
+// ===================================================================================================
+
+var userLength = parseInt(userLength);
+var invalidLength = userLength < 8 || userLength > 128;
+
+validateLength = () => {
+  userLength = prompt(
     "How many characters do you want? Choose between 8 - 128."
   );
-
-  userLength = parseInt(userLength);
-  var isValidLength = userLength < 8 || userLength > 128;
-
-  // create conditions for user inputted characters.
-
-  if (isValidLength) {
-    generatePassword(); //starts the process over again
+  invalidLength = userLength < 8 || userLength > 128;
+  if (invalidLength) {
+    validateLength(); //starts the process over again
   } else {
     userPrompts(); //this is the main function that runs everything
   }
+};
 
-  // userPrompts main function below, along with conditionals for for loop_____________________________________________ //
+// RUNS GENERATEPASSWORD FUNCTION
+// ===================================================================================================
+function generatePassword() {
+  validateLength();
+}
 
-  function userPrompts() {
-    // prompt user to decide yes/no for special characters
-    var userSpecialCharacters = confirm(
-      "Click Okay if you want to use special characters, or else click cancel"
-    );
-    console.log(userSpecialCharacters);
+// PROMPTS USER TO DECIDE ON SPECIAL CHARACTERS
+// ===================================================================================================
+specialChar = () => {
+  userSpecialCharacters = confirm(
+    "Click Okay if you want to use special characters, or else click cancel"
+  );
+  // defaults to checking whether the statement below is true
+  if (userSpecialCharacters) {
+    nestedArr.push(specCharArr);
+  }
+};
 
-    // defaults to checking whether the statement below is true
-    if (userSpecialCharacters) {
-      nestedArr.push(specCharArr);
-    }
+// PROMPTS USER TO DECIDE ON NUMBER CHARACTERS
+// ===================================================================================================
+numberChar = () => {
+  userNumbers = confirm(
+    "Click Okay if you want to use number characters, or else click cancel"
+  );
+  // defaults to checking whether the statement below is true
+  if (userNumbers) {
+    nestedArr.push(numbersArr);
+  }
+};
 
-    // prompt user to decide yes/no for numbers
-    var userNumbers = confirm(
-      "Click Okay if you want to use number characters, or else click cancel"
-    );
-    console.log(userNumbers);
+// PROMPTS USER TO DECIDE ON LOWERCASE CHARACTERS
+// ===================================================================================================
+lowercaseChar = () => {
+  userLowerCase = confirm(
+    "Click Okay if you want to use lowercase characters, or else click cancel"
+  );
+  // defaults to checking whether the statement below is true
+  if (userLowerCase) {
+    nestedArr.push(lowerCaseArr);
+  }
+};
 
-    // defaults to checking whether the statement below is true
-    if (userNumbers) {
-      nestedArr.push(numbersArr);
-    }
+// PROMPTS USER TO DECIDE ON UPPERCASE CHARACTERS
+// ===================================================================================================
+uppercaseChar = () => {
+  userUpperCase = confirm(
+    "Click Okay if you want to use uppercase characters, or else click cancel"
+  );
+  // defaults to checking whether the statement below is true
+  if (userUpperCase) {
+    nestedArr.push(upperCaseArr);
+  }
+};
 
-    // prompt user to decide yes/no for lower case letters
-    var userLowerCase = confirm(
-      "Click Okay if you want to use lowercase characters, or else click cancel"
-    );
-    console.log(userLowerCase);
+// RUNS USERPROMPTS FUNCTION
+// ===================================================================================================
+function userPrompts() {
 
-    // defaults to checking whether the statement below is true
-    if (userLowerCase) {
-      nestedArr.push(lowerCaseArr);
-    }
-
-    // prompt user to decide yes/no for upper case letters
-    var userUpperCase = confirm(
-      "Click Okay if you want to use uppercase characters, or else click cancel"
-    );
-    console.log(userUpperCase);
-
-    // defaults to checking whether the statement below is true
-    if (userUpperCase) {
-      nestedArr.push(upperCaseArr);
-    }
-
-    // if user chooses nothing, iterate over cycle to give user another chance to input correctly
-    if (
-      userSpecialCharacters === false &&
-      userNumbers === false &&
-      userLowerCase === false &&
-      userUpperCase === false
-    ) {
-      alert("Please okay a criterion least once");
-      userPrompts();
-    }
-
-    // if criterion meets true from user prompts (i.e., confirm if you want special characters)
-    // iterate through loop of such array
-    // if criterion not true
-    // move on to next
-
-    // generate a random number using math floor math random methods to iterate over four arrays
-    // select random index of array
-    // select random index of inner array
-    // concat (push) such character to userPassword (push characters into new string)
-
-    var randomNumber1 = Math.floor(Math.random() * nestedArr.length);
-    var randomNumber2 = Math.floor(
-      Math.random() * nestedArr[randomNumber1].length
-    );
-
-    var userPassword = "";
-
-    for (var i = 0; i < userLength; i++) {
-      userPassword = userPassword + nestedArr[randomNumber1][randomNumber2];
-      randomNumber1 = Math.floor(Math.random() * nestedArr.length);
-      randomNumber2 = Math.floor(
-        Math.random() * nestedArr[randomNumber1].length
-      );
-    }
-    return console.log(userPassword);
+  specialChar();
+  numberChar();
+  lowercaseChar();
+  uppercaseChar();
+  
+  // if user chooses nothing, iterate over cycle to give user another chance to input correctly
+  
+  if (
+    userSpecialCharacters === false &&
+    userNumbers === false &&
+    userLowerCase === false &&
+    userUpperCase === false
+  ) {
+    alert("Please choose at least one criterion");
+    userPrompts();
+  } else {
+    passwordGenerator();
   }
 }
+
+// RUNS PASSWORDGENERATOR FUNCTION
+// ===================================================================================================
+
+var userPassword = "";
+var randomNumber1;
+var randomNumber2;
+
+passwordGenerator = () => {
+  randomNumber1 = Math.floor(Math.random() * nestedArr.length);
+  randomNumber2 = Math.floor(
+    Math.random() * nestedArr[randomNumber1].length
+  );
+
+  userPassword = "";
+
+  for (var i = 0; i < userLength; i++) {
+    userPassword = userPassword + nestedArr[randomNumber1][randomNumber2];
+    randomNumber1 = Math.floor(Math.random() * nestedArr.length);
+    randomNumber2 = Math.floor(Math.random() * nestedArr[randomNumber1].length);
+  }
+  return console.log(userPassword);
+}
+
+  // if criterion meets true from user prompts (i.e., confirm if you want special characters)
+  // iterate through loop of such array
+  // if criterion not true
+  // move on to next
+
+  // generate a random number using math floor math random methods to iterate over four arrays
+  // select random index of array
+  // select random index of inner array
+  // concat (push) such character to userPassword (push characters into new string)
+
+// Need to return the password into the text area.
+// Need to copy to clipboard once button is clicked.
+
+
+
+
+
+
+
+
+
+
